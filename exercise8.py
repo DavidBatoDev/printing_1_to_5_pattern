f61c7cbca94ebb04140f9fc89ccad697dcd25dd2
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 15:28:49 +0800

    Finished Code!

--- exercise8.py
+++ exercise8.py
@@ -5,6 +5,13 @@
 # 4 4 4 4 
 # 5 5 5 5 5
 # define a for loop with a range of 1 to 6
+for i in range(1, 6):
 # for each iteration, convert the iteraton to str 
-# and print based on the number of that iteratiopn
+    converted_num_to_str = str(i)
+# and print based on the number of that iteration
+    print(converted_num_to_str * i)
+
+
+# Done!!
+
 
