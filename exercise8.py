2e1c3c2765331f09ea56253908d56e9155babb93
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 15:27:39 +0800

    Initialize Commit and done with pseudocode

---
+++ exercise8.py
@@ -0,0 +1,10 @@
+# Expected Output
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
+# define a for loop with a range of 1 to 6
+# for each iteration, convert the iteraton to str 
+# and print based on the number of that iteratiopn
+
